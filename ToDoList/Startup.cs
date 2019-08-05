18759cd91f74fc71c51852668e4207d28e245567
Jessica Munoz <ms.jessica.munoz@gmail.com>
2019-08-04 22:27:15 -0700

    Added Catagories object and converted Items object to be inside of Catagories. Added database and began testing.

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -6,33 +6,38 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace ToDoList
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IHostingEnvironment env)
     {
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddEnvironmentVariables();
-            Configuration= builder.Build();
-        }
-        public IConfigurationRoot Configuration {get; }
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-        }
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name:"default",
-                    template:"{controller=Home}/{action=Index}/{id}");
-            });
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("something went wrong!");
-            });
-        }
+      var builder = new ConfigurationBuilder()
+          .SetBasePath(env.ContentRootPath)
+          .AddEnvironmentVariables();
+      Configuration = builder.Build();
     }
+    public IConfigurationRoot Configuration { get; }
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+    }
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseDeveloperExceptionPage();
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute(
+                  name: "default",
+                  template: "{controller=Home}/{action=Index}/{id}");
+      });
+      app.Run(async (context) =>
+      {
+        await context.Response.WriteAsync("something went wrong!");
+      });
+    }
+  }
+
+  public static class DBConfiguration
+  {
+    public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
+  }
 }
